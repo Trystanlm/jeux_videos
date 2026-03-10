51ae4b349e6eb5b3f8a40891accdeee3836967b1
Trystanlm <trystan5lemarec@gmail.com>
2026-03-10 13:42:04 -0400

    jsp

--- TP1_Recherche/Assets/Scripts/S_Player.cs
+++ TP1_Recherche/Assets/Scripts/S_Player.cs
@@ -7,10 +7,12 @@ public class S_Player : MonoBehaviour
 
     Vector2 direction = Vector2.zero;
     float vitesse = 0;
-    float acceleration = 0.2f;
+    float acceleration = 0.3f;
 
     Animator animator;
 
+
+    int nbKeys = 0;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -33,5 +35,20 @@ public class S_Player : MonoBehaviour
         vitesse = Mathf.Clamp(vitesse, 0, 1);
 
         animator.SetFloat("Weight", vitesse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Key"))
+        {
+            nbKeys++;
+            Destroy(collision.gameObject);
+        }
+
+        if(collision.gameObject.CompareTag("Chest") && nbKeys > 0)
+        {
+            nbKeys--;
+            Destroy(collision.gameObject);
+        }
     }
 }
